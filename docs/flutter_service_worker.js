c368e0f31322524dd773880078c25eac652aab1f
Mohammadsofy <mohammadsofyan079@gmail.com>
2025-10-08 21:09:08 +0300

    refresh

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "074166f709d06ba572053e36b4cb5bc7
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "2720b6f3cc2c13bd79087c896ba60527",
+"flutter_bootstrap.js": "c9e450ddbc83aa71a6e6463bc446477e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
